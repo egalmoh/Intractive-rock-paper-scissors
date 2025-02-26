21784e9da5ec12bcad9e690eb2cc2354a2ffcc26
Maxamed Cigaal <lovemoha47@gmail.com>
2025-02-26 16:51:15 +0300

    added html and js files

---
+++ rock-paper-scissors.js
@@ -0,0 +1,87 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+/*
+if (!score) {
+    score = {
+        wins: 0,
+        losses: 0,
+        ties: 0 
+    }
+}
+*/
+
+function playGame(playerMove) {
+    const computerMove = pickComputerMove();
+    const p_text = document.getElementById('para-text');
+    let wins = document.getElementById('wins');
+    let losses = document.getElementById('losses');
+    let ties = document.getElementById('ties')
+
+    let result = '';
+
+    if (playerMove === '✊') {
+        
+        if (computerMove === '✊'){
+            result = 'Tie.';
+        } else if (computerMove === '✋'){
+            result = 'You lose.';
+        } else if (computerMove === '✌️'){
+            result = 'You win.';
+        }
+    } else if (playerMove === '✋'){
+        
+        if (computerMove === '✋'){
+            result = 'Tie.';
+        } else if (computerMove === '✊'){
+            result = 'You win.';
+        } else if (computerMove === '✌️'){
+            result = 'You lose.';
+        }
+    } else if (playerMove === '✌️'){
+        
+        if (computerMove === '✌️'){
+            result = 'Tie.';
+        } else if (computerMove === '✋'){
+            result = 'You win.';
+        } else if (computerMove === '✊'){
+            result = 'You lose.';
+        }
+    }
+
+    if (result === "You win."){
+        score.wins++;
+    } else if (result === "You lose."){
+        score.losses++;
+    } else if (result === "Tie."){
+        score.ties++;
+    }
+
+    localStorage.setItem('score', JSON.stringify(score))
+
+    wins.innerHTML = `Wins: ${score.wins}`;
+    losses.innerHTML = `Losses: ${score.losses}`;
+    ties.innerHTML = `Ties: ${score.ties}`;
+    
+    p_text.innerHTML = `You picked ${playerMove}. Computer picked ${computerMove}. ${result}`;
+}
+
+
+function pickComputerMove() {
+    const randomNumber = Math.random();
+    console.log(randomNumber)
+    let computerMove = '';
+
+    if (randomNumber >= 0 && randomNumber < 1/3){
+        computerMove = '✊';
+    } else if (randomNumber >= 1/3 && randomNumber < 2/3){
+        computerMove = '✋';
+    } else if (randomNumber >= 2/3 && randomNumber < 1){
+        computerMove = '✌️'
+    }
+
+    return computerMove
+}
